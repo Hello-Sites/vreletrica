2610fb56c211039b4cd4f170fd2c5c13e0ba0648
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-02-11 22:29:13 +0000

    Muitas coisas

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
-function headerColor(){
-    const header = document.getElementById('page-header')
-    const banner = document.getElementById('page-banner')
+function headerColorControl({headerId, bannerId, normal, inverted}){
 
-    const inverseElement = Array.from(header.children)
+    const header = document.getElementById(headerId)
+    const banner = document.getElementById(bannerId) || header.parentElement
+
+    const color = { false: normal, true: inverted }
 
     document.addEventListener('scroll', () => {
-        if(banner.offsetHeight >  window.pageYOffset + header.offsetHeight){
-            changeBackgroundColorWithFade({
-                element: header,
-                from: getComputedStyle(header, null).getPropertyValue("background-color"), to: "#fff" })
 
-            for(const elem of inverseElement){
-                elem.classList.remove('inverted')
+        const isBelow = banner.offsetHeight <  window.pageYOffset + header.offsetHeight
+
+        changeBackgroundColor({
+            element: header,
+            from: getComputedStyle(header, null).getPropertyValue("background-color"), to: color[isBelow] })
+
+
+        for (const child of Array.from(document.getElementsByClassName('inverse-'+headerId)) ){
+            if(isBelow){
+                child.classList.add('inversed')
+            }else{
+                child.classList.remove('inversed')
             }
 
-        }else{
-            changeBackgroundColorWithFade({
-                element: header,
-                from: getComputedStyle(header, null).getPropertyValue("background-color"), to: "#242424" })
-
-            for(const elem of inverseElement){
-                elem.classList.add('inverted')
-            }
         }
+
     })
 }
 
 
 
-function changeBackgroundColorWithFade({from, to, element})  {
+function changeBackgroundColor({from, to, element})  {
     element.animate([ {backgroundColor: from}, {backgroundColor: to}],
     {
         duration: 200,
@@ -38,16 +38,35 @@ function changeBackgroundColorWithFade({from, to, element})  {
 }
 
 function smoothScroll(){
-    const items = document.getElementsByClassName('smooth-scroll')
-    for(const item of items){
-        item.addEventListener('click', scrollToObject)
+    const items = {
+        links: document.getElementsByTagName('a'),
+        buttons: document.getElementsByTagName('button'),
+        generic: document.getElementsByClassName('smooth-scroll')
+    }
+
+    console.log(items)
+
+    for(const type in items){
+        for(const item of items[type]){
+            const targetId = item.getAttribute('onclick') ||  item.getAttribute('href') || item.getAttribute('goto')
+            if(targetId && targetId !== "#"){
+                item.smoothScrollTarget = targetId
+                item.addEventListener('click', scrollToObject)
+            }else{
+                item.smoothScrollTarget = "#top"
+                console.warn('Scroll: Target is '+targetId+':', item.outerHTML, '\n', Array.from(document.all).indexOf(item) + 'º Element')
+            }
+        }
     }
 
     function scrollToObject(event){
         event.preventDefault()
-        const element = event.target.parentElement
-        const href = element.getAttribute('href').slice(1)
-        const target = document.getElementById(href).offsetTop
+
+        const element = event.originalTarget.smoothScrollTarget? event.originalTarget: false
+        const targetElement = document.getElementById(element.smoothScrollTarget.slice(1))
+        const target = targetElement? targetElement.offsetTop : 0
+
+        console.info('goning to:', element.smoothScrollTarget)
 
         const header = document.getElementById('page-header')
         const padding = header? header.clientHeight : 0
@@ -59,6 +78,26 @@ function smoothScroll(){
 
     }
 }
-smoothScroll()
-headerColor()
+
+function controllAppearSections() {
+    const sections = document.getElementsByClassName('start-hidden')
+    for(const section of sections) {
+        const displayKey = getComputedStyle(section, null).display
+        section.displaySave = displayKey
+        section.top = section.offsetTop
+        section.style.display = 'none'
+        sections.items = section
+    }
+
+    document.onwheel = checkAndDisplay
+
+    function checkAndDisplay(){
+        const scrolled = (document.getElementById('page-banner').getBoundingClientRect().y * -1) + window.innerHeight
+        for(const section of sections){
+            if(scrolled >= section.top){
+               section.style.display = section.displaySave
+            }
+        }
+    }
+}
 
